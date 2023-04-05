aded52ed731a193ea1ba46146da21ccea96a67fe
DRGWeiss <ruizd1710@gmail.com>
2023-04-05 06:26:58 -0400

    database and user CRUD

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config({ path: './.env' })
+const cors = require('cors')
+const app = express();
+const PORT = 3002;
+const createError = require('http-errors')
+
+const usersRouter = require('./routes/users')
+
+
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/users', usersRouter)
+
+
+
+const { connectDB } = require('./mongodb')
+connectDB()
