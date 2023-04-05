aee9edb85d8d8796eee5b7039a765c6749767895
DRGWeiss <ruizd1710@gmail.com>
2023-04-05 08:20:29 -0400

    authentication for application routes

--- routes/applications.js
+++ routes/applications.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const applicationController = require('../controllers/application')
+var authenticate = require('../authenticateMiddleware')
 
-router.get('/user/:userid', applicationController.getApplications)
-router.get('/:id', applicationController.getApplication)
-router.post('/', applicationController.create)
-router.patch('/', applicationController.update)
-router.delete('/:id', applicationController.delete)
+router.get('/user/:userid', authenticate.authenticateToken, applicationController.getApplications)
+router.get('/:id', authenticate.authenticateToken, applicationController.getApplication)
+router.post('/', authenticate.authenticateToken, applicationController.create)
+router.patch('/', authenticate.authenticateToken, applicationController.update)
+router.delete('/:id', authenticate.authenticateToken, applicationController.delete)
 
 module.exports = router
